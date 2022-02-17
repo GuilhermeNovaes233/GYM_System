01373cf75cfdb346e872530c9ac487a3cf5df123
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2022-02-17 12:33:54 -0300

    Adicionando propriedades na view model de professor

--- GYM_System.Application/ViewModels/TeacherViewModel.cs
+++ GYM_System.Application/ViewModels/TeacherViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using GYM_System.Domain.Entities;
+using GYM_System.Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GYM_System.Application.ViewModels
 {
     public class TeacherViewModel
     {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string ZipCode { get; private set; }
+        public Address Address { get; private set; }
+        public string CPF { get; private set; }
+        public string RG { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Photo { get; private set; }
+        public string Wage { get; private set; }
+        public string Entry_time { get; private set; }
+        public string End_time { get; private set; }
+        public string Especiality { get; private set; }
+        public IEnumerable<Student> Students { get; private set; }
     }
 }
